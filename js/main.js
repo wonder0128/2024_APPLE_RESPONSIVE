096c68067582a73ac4836decedb6a9468b418443
roze <wonder_0128@naver.com>
2024-01-08 09:24:09 +0900

    20240108

--- js/main.js
+++ js/main.js
@@ -30,19 +30,41 @@ function hideBasket() {
 
 // search
 const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]; // 전개 연산자를 이용한 얕은 복사
 const searchWrapEl = headerEl.querySelector('.search-wrap');
 const searchStarterEl = headerEl.querySelector('.search-starter');
 const searchCloserEl = searchWrapEl.querySelector('.search-closer');
 const shadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
-searchStarterEl.addEventListener('click', showSearching);
-searchCloserEl.addEventListener('click', hideSearching);
-shadowEl.addEventListener('click', hideSearching);
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+shadowEl.addEventListener('click', hideSearch);
 
-function showSearching() {
-    headerEl.classList.add('.searching');
+function showSearch() {
+    headerEl.classList.add('searching');
+    document.documentElement.classList.add('fixed');
+    headerMenuEls.reverse().forEach((el, idx) => {
+        el.style.transitionDelay = (idx * .4) / headerMenuEls.length + 's';
+    });
+    searchDelayEls.forEach((el, idx) => {
+        el.style.transitionDelay = (idx * .4) / searchDelayEls.length + 's';
+    });
+    setTimeout(() => {
+        searchInputEl.focus();
+    }, 600);
 }
 
-function hideSearching() {
-    headerEl.classList.remove('.searching');
+function hideSearch() {
+    headerEl.classList.remove('searching');
+    document.documentElement.classList.remove('fixed');
+    headerMenuEls.reverse().forEach((el, idx) => {
+        el.style.transitionDelay = (idx * .4) / headerMenuEls.length + 's';
+    });
+    searchDelayEls.reverse().forEach((el, idx) => {
+        el.style.transitionDelay = (idx * .4) / searchDelayEls.length + 's';
+    });
+    searchDelayEls.reverse();
+    searchInputEl.value = '';
 }
