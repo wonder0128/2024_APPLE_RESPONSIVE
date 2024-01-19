dd032eb7debbbd6d787c7b9d5f43521acff4d19f
roze <wonder_0128@naver.com>
2024-01-19 16:10:57 +0900

    20240119 - end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
+
 // basket
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
@@ -83,3 +86,79 @@ const infoEls = document.querySelectorAll('.info');
 infoEls.forEach(el => {
     io.observe(el);
 })
+
+//  비디오 재생
+const video = document.querySelector('.stage video');
+const playBtn = document.querySelector('.stage .controller--play');
+const pauseBtn = document.querySelector('.stage .controller--pause');
+
+playBtn.addEventListener('click', () => {
+    video.play();
+    playBtn.classList.add('hide');
+    pauseBtn.classList.remove('hide');
+});
+pauseBtn.addEventListener('click', () => {
+    video.pause();
+    pauseBtn.classList.add('hide');
+    playBtn.classList.remove('hide');
+});
+
+// '당신에게 맞는 iPad는?' 렌더링
+const itemsEl = document.querySelector('section.compare .items');
+ipads.forEach(ipad => {
+    const itemEl = document.createElement('div');
+    itemEl.classList.add('item');
+
+    let colorList = '';
+
+    ipad.colors.forEach(color => {
+        colorList += `<li style="background-color: ${color};"></li>`
+    })
+
+    itemEl.innerHTML = /* html */ `
+        <div class="thumbnail">
+            <img src="${ipad.thumbnail}" alt="${ipad.name}">
+        </div>
+        <ul class="colors">
+            ${colorList}
+        </ul>
+        <h3 class="name">${ipad.name}</h3>
+        <p class="tagline">${ipad.tagline}</p>
+        <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+        <button class="btn">구입하기</button>
+        <a href="${ipad.url}" class="link">더 알아보기</a>
+    `;
+    
+    itemsEl.append(itemEl);
+})
+
+const navigationsEl = document.querySelector('footer .navigations');
+navigations.forEach(nav => {
+    const mapEl = document.createElement('div');
+    mapEl.classList.add('map');
+
+    let mapList = '';
+
+    nav.maps.forEach(map => [
+        mapList += /* html */ `
+        <li>
+            <a href="${map.url}">${map.name}</a>
+        </li>
+        `
+    ])
+
+    mapEl.innerHTML = /* html */ `
+        <h3>
+            <span class="text">${nav.title}</span>
+        </h3>
+        <ul>
+            ${mapList}
+        </ul>
+    `;
+
+    navigationsEl.append(mapEl);
+})
+
+// 올해의 연도
+const thisyearEl = document.querySelector('span.this-year');
+thisyearEl.textContent = new Date().getFullYear();
